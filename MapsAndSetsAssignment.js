5889d83d2a667b2976b4dc3fe419ac195bc8c7cc
cafemanager <cafemanager97@gmail.com>
2022-12-18 01:06:27 -0800

    added Maps and Sets assignment

---
+++ MapsAndSetsAssignment.js
@@ -0,0 +1,56 @@
+// Quick Question #1
+// What does the following code return?
+
+// new Set([1,1,2,2,3,4])
+
+let answer1 = "{1,2,3,4}";
+
+// Quick Question #2
+// What does the following code return?
+
+// [...new Set("referee")].join("")
+let answer2 = "ref";
+
+// Quick Questions #3
+// What does the Map m look like after running the following code?
+
+// let m = new Map();
+// m.set([1,2,3], true);
+// m.set([1,2,3], false);
+let answer3 = `0: [1,2,3] => true \n 1: [1,2,3] => false`;
+
+// hasDuplicate
+// Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
+
+// hasDuplicate([1,3,2,1]) // true
+// hasDuplicate([1,5,-1,4]) // false
+function hasDuplicate(arr){
+    if(arr.length === new Set(arr).size){
+        return false;
+    }
+    return true;
+}
+
+// vowelCount
+// Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the string.
+
+// vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+// vowelCount('Colt') // Map { 'o' => 1 }
+
+function vowelCount(str){
+    let hashMap = new Map();
+    console.log(str)
+    for(let x in str){
+        let strVal = str[x];
+        console.log(x)
+        console.log(strVal)
+        if("aeiou".indexOf(strVal) !== -1){
+            if(hashMap.get(strVal) === undefined){
+                hashMap.set(strVal, 1)
+            }
+            else{ hashMap.set(strVal, hashMap.get(strVal)+1) }
+        }
+    }
+    return hashMap;
+
+}
