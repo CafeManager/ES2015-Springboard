8d074ed3a53f8be5dbb9c4df227b9d2718936a5f
cafemanager <cafemanager97@gmail.com>
2022-12-17 16:17:43 -0800

    added destructuring assignment

---
+++ DestructuringAssignment.js
@@ -0,0 +1,104 @@
+/* 
+Object Destructuring 1
+What does the following code return/print?
+
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // ?
+console.log(yearNeptuneDiscovered); // ?
+*/
+
+let answer1 = "8"
+
+/*
+Object Destructuring 2
+What does the following code return/print?
+
+let planetFacts = {
+  numPlanets: 8,
+  yearNeptuneDiscovered: 1846,
+  yearMarsDiscovered: 1659
+};
+
+let {numPlanets, ...discoveryYears} = planetFacts;
+
+console.log(discoveryYears); // ?
+*/
+
+let answer2 = {
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+}
+
+/*
+Object Destructuring 3
+What does the following code return/print?
+
+function getUserData({firstName, favoriteColor="green"}){
+  return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+
+getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // ?
+getUserData({firstName: "Melissa"}) // ?
+getUserData({}) // ?
+*/
+
+let answer3a = `Your name is Alejandro and you like purple`;
+let answer3b = `Your name is Melissa and you like green`;
+let answer3c = `Your name is undefined and you like green`;
+
+/*
+Array Destructuring 1
+What does the following code return/print?
+
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // ?
+console.log(second); // ?
+console.log(third); // ?
+*/
+
+let answer4a = "Maya";
+let answer4b = "Marisa";
+let answer4c = "Chi";
+
+/*
+Array Destructuring 2
+What does the following code return/print?
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+  "Raindrops on roses",
+  "whiskers on kittens",
+  "Bright copper kettles",
+  "warm woolen mittens",
+  "Brown paper packages tied up with strings"
+]
+
+console.log(raindrops); // ?
+console.log(whiskers); // ?
+console.log(aFewOfMyFavoriteThings); // ?
+*/
+
+let answer5a = "Raindrops on roses";
+let answer5b = "whiskers on kittens";
+let answer5c = [
+                "Bright copper kettles",
+                "warm woolen mittens",
+                "Brown paper packages tied up with strings"
+                ];
+
+/*
+Array Destructuring 3
+What does the following code return/print?
+
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // ?
+*/
+
+let answer6 = [10, 30, 20];
+
+// ES2015 Refactoring
+
